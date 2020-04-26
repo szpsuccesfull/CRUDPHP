aabf474e302adc748201e4079405bdee1b88e50a
samuel zapata <samuel.zapata0111@gmail.com>
2020-04-25 20:23:37 -0500

    me falta el formulario

--- BaseDatos.php
+++ BaseDatos.php
@@ -23,6 +23,10 @@ public class BaseDatos{
         );
 
         // validar el estado de la funciones 
+        if(!($this->conexion)){
+            die('<br> Error de conexion('.mysqli_connect_errno().')'.mysqli_connect_error());           
+        }else{
+            echo "<br> coneccion exitosa a la base de datos".$this->nombreBaseDatos;
     } 
 
 }
